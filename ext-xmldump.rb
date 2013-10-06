a224e544b543c4724987585293efd5b12677a0d0
Masao Takaku <tmasao@acm.org>
2013-10-06 15:17:27 +0900

    an extraction script for Wikipedia dumps

---
+++ ext-xmldump.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require "pp"
+require "rubygems"
+require "libxml"
+
+class MyWikipediaDumps
+   include LibXML::XML::SaxParser::Callbacks
+   def initialize
+      @context = {}
+   end
+   def on_start_element( element, attributes )
+      #puts "Element started: #{element}"
+      case element
+      when "page"
+         @context = {}
+      when "title", "text", "ns"
+         @context[ :state ] = element
+      end
+   end
+   def on_end_element( element )
+      case element
+      when "page"
+         output
+      when "title", "text", "ns"
+         @context.delete( :state )
+      end
+   end
+   def on_characters( str )
+      #p @context[ :state ]
+      @context[ @context[ :state ] ] = str
+   end
+
+   def output
+      if @context[ "ns" ] == "0"
+         puts @context[ "title" ]
+      end
+   end
+end
+
+if $0 == __FILE__
+   #parser = LibXML::XML::Parser.io( ARGF )
+   parser = LibXML::XML::SaxParser.io( ARGF )
+   parser.callbacks = MyWikipediaDumps.new
+   parser.parse
+   #doc = parser.parse
+   #doc.root.namespaces.default_prefix = 'mw'
+   #pages = doc.find( "//mw:page" )
+   #puts pages.size
+
+#    pages.each do |page|
+#       namespace = page.find( "./mw:ns" )[0].content.to_i
+#       title = page.find( "./mw:title" )[0].content
+#       unless namespace != 0
+#          STDERR.puts title
+#          next
+#       end
+#       text = page.find( ".//mw:text" )[0].content
+#       puts [ title, text.size ].join( "\t" )
+#    end
+end
