214633ac30879390bbbcf0464cfa1f6591488afe
Masao Takaku <tmasao@acm.org>
2013-11-10 23:56:28 +0900

    changed "editsection" pattern.

--- spec/mediawiki-parser_spec.rb
+++ spec/mediawiki-parser_spec.rb
@@ -3,9 +3,22 @@
 
 require "mediawiki-parser.rb"
 
-describe "cinii.rb" do
-   it "should parse properly the page '言語'" do
-      parser = MediaWikiParser::Kiwi.new( '言語' )
-      parser.to_html
+describe "mediawiki-parser.rb" do
+   context "#to_html" do
+      it "should parse properly the page '言語'." do
+         parser = MediaWikiParser::Kiwi.new( '言語' )
+         html = parser.to_html
+         html.should match /<b>言語<\/b>/
+      end
+      it "should not include Editsection in the final contents." do
+         parser = MediaWikiParser::Kiwi.new( '言語' )
+         html = parser.to_html
+         html.should_not match /<span class="editsection">/
+      end
+      it "should support 'ファイル:' namespace for images." do
+         parser = MediaWikiParser::Kiwi.new( 'オリエンテーリング' )
+         html = parser.to_html
+         html.should_not match /thumb(nail)?\|/
+      end
    end
 end
