8db7dade15cdef669fb9f773891a50c9902efeae
Masao Takaku <tmasao@acm.org>
2015-05-22 13:38:18 +0000

    reranking method added.

--- solr.rb
+++ solr.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # $Id$
 
+require "set"
+require "pp"
 require "rubygems"
 require "rsolr"
 
@@ -32,9 +34,31 @@ class WikipediaSolr
       params.update( opts )
       response = @solr.get( "select", :params => params )
    end
+
+   def reranking( response, params = { :alpha => 0.5 } )
+     alpha = params[ :alpha ]
+     docs = response[ "response" ][ "docs" ]
+     results = []
+     results << docs.shift
+     until docs.empty? do
+       previous_categories = results.map{|e| e[ "category" ] }.flatten.uniq
+       docs.sort_by! do |e| 
+         score = e[ "score" ].to_f
+         category = e[ "category" ]
+	 if category.empty?
+	   cat_score = 0.1
+	 else
+	   cat_score = ( Set[ *previous_categories ] & Set[ *category ] ).size / category.size.to_f
+         end
+         alpha * score * ( 1-alpha ) * cat_score
+       end
+       results << docs.shift
+     end
+     results
+   end
 end
 
 if $0 == __FILE__
   solr = WikipediaSolr.new
-  solr.add( { :title => "test" } )
+  #solr.add( { :title => "test" } )
 end
