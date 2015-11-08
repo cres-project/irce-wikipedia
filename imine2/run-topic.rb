d4c07dd994a9f2340f56f9d3a070bfe09a41f3fd
Masao Takaku <tmasao@acm.org>
2015-11-08 06:06:57 +0000

    output querying command.

--- imine2/run-topic.rb
+++ imine2/run-topic.rb
@@ -16,5 +16,12 @@ if $0 == __FILE__
     exit
   end
   command = ARGV[0]
-  topics = open(ARGV[1]){io| io.readlines }
+  topics = open(ARGV[1]) do |io|
+    io.readlines
+  end
+  topics.each do |line|
+    topic_id, query, = line.chomp.split(/\t/)
+    puts "## #{ topic_id }\t#{ query }"
+    system "#{ command } #{ query }"
+  end
 end
