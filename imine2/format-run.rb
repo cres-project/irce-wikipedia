d3c49fa87aca5d50c1c6e5d3d3ad28267c12c2d1
Masao Takaku <tmasao@acm.org>
2015-11-19 18:43:11 +0000

    remove duplicate subtopics.

--- imine2/format-run.rb
+++ imine2/format-run.rb
@@ -12,15 +12,19 @@ if $0 == __FILE__
       query = $2.dup
     else
       topic, score, = line.chomp.split(/\t/)
-      next if done[query_id] and done[query_id] >= 10
+      next if done[query_id] and done[query_id].size >= 10
       next if topic == query
       next if query =~ /#{topic}/i
+      if topic == "存命人物"
+        topic = "人物"
+      end
       unless topic.include?( query ) 
         topic = [ query, topic ].join(" ")
       end
+      next if done[query_id] and done[query_id].include?(topic)
       puts [ query_id, topic, nil, score, run_name ].join("\t")
-      done[query_id] ||= 0
-      done[query_id] += 1
+      done[query_id] ||= []
+      done[query_id] << topic
     end
   end
 end
