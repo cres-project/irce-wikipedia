d5c4796139923dbcce42318838c88386d9c06212
Masao Takaku <tmasao@acm.org>
2015-11-19 14:04:10 +0000

    output top 10 subtopics only. fix columns.

--- imine2/format-run.rb
+++ imine2/format-run.rb
@@ -3,13 +3,17 @@
 if $0 == __FILE__
   run_name = ARGV.shift
   query_id = nil
+  done = {}
   ARGF.each do |line|
     case line
     when /\A## ([\w\-]+)/
       query_id = $1.dup
     else
       topic, score, = line.chomp.split(/\t/)
-      puts [ run_name, query_id, topic, nil, score ].join("\t")
+      next if done[query_id] and done[query_id] >= 10
+      puts [ query_id, topic, nil, score, run_name ].join("\t")
+      done[query_id] ||= 0
+      done[query_id] += 1
     end
   end
 end
